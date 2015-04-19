a2531df63a3a20b9939412f6a154df44f92397af
Dung Tran <tad88.dev@gmail.com>
2015-04-19 15:28:37 +0700

    + Add base zt javascript framework object

--- framework/bootstrap.php
+++ framework/bootstrap.php
@@ -14,7 +14,18 @@ require_once __DIR__ . '/includes/framework.php';
 $ztPath = ZtPath::getInstance();
 $ztPath->registerNamespace('Zt',__DIR__);
 
-echo '<pre>';
-print_r ($ztPath);
-echo '</pre>';
+//echo '<pre>';
+//print_r ($ztPath);
+//echo '</pre>';
+
+/* Include JS Framework core */
+if(JFactory::getApplication()->input->get('zt.ajax', false) === false){
+    JHtml::_('bootstrap.framework');
+    ob_start();
+    require_once $ztPath->getInstance()->getPath('Zt://assets/js/zt.core.php');
+    $script = ob_get_contents();
+    ob_end_clean();
+    JFactory::getDocument()->addScriptDeclaration($script);
+}
+
 spl_autoload_register(array('ZtLoader', 'autoloadZtPsr2'));
--- framework/includes/defines.php
+++ framework/includes/defines.php
@@ -6,5 +6,6 @@
 defined('_JEXEC') or die('Restricted access');
 
 define('ZTFRAMEWORK', 1);
+define('ZTVERSION', '0.0.0');
 
 define('ZT_PATH_ROOT', realpath(__DIR__ . '/../'));
