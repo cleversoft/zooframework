16fa4c2411c1eef4a2b0745693f24c65ffae41a7
Viet Vu <jooservices@gmail.com>
2015-04-20 11:39:10 +0700

    - remove test code

--- framework/bootstrap.php
+++ framework/bootstrap.php
@@ -12,9 +12,6 @@ require_once __DIR__ . '/includes/framework.php';
 
 /* Register Zo2 autoloading by Psr2 */
 $ztPath = ZtPath::getInstance();
-$ztPath->registerNamespace('Zt',__DIR__);
+$ztPath->registerNamespace('Zt', __DIR__);
 
-echo '<pre>';
-print_r ($ztPath);
-echo '</pre>';
 spl_autoload_register(array('ZtLoader', 'autoloadZtPsr2'));
