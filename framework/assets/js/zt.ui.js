fff3f727cea1d5306f885831ee3f6c487be4a6cf
Dung Tran <tad88.dev@gmail.com>
2015-04-22 16:39:29 +0700

    + Add base zt.ui function

---
+++ framework/assets/js/zt.ui.js
@@ -0,0 +1,58 @@
+/**
+ * Zt Ui
+ * @param {type} w
+ * @param {type} z
+ * @param {type} $
+ * @returns {undefined}
+ */
+jQuery(document).ready(function(){
+    
+(function (w, z, $) {
+    /* Reject if zt is not defined */
+    if (typeof (z) === 'undefined')
+        return false;
+
+    /* Local ajax class */
+    var _ui = {
+        /* Local settings */
+        _settings: {},
+        /**
+         * Init function
+         * @returns {undefined}
+         */
+        _init: function () {
+        },
+        /**
+         * Replace HTML content inside element
+         * @param {type} el
+         * @param {type} html
+         * @returns {undefined}
+         */
+        replace: function(el, html){
+            $(el).html(html);
+        },
+        /**
+         * Append HTML at last element
+         * @param {type} el
+         * @param {type} html
+         * @returns {undefined}
+         */
+        append: function(el, html){
+            $(el).append(html);
+        },
+        /**
+         * Raise message
+         * @param {type} messageSettings
+         * @returns {undefined}
+         */
+        raiseMessage: function(messageSettings){
+        }
+    };
+
+    /* Append to Zt JS Framework */
+    z.ui = _ui;
+    z.ui._init();
+    
+})(window, zt, zt.$);
+
+});
