a79d2a9c9bcb8f6a1cc9ec428b461fb3e848a9c8
Viet Vu <jooservices@gmail.com>
2015-05-14 09:59:42 +0700

    ^ code clean up

--- zt.php
+++ zt.php
@@ -8,29 +8,36 @@ defined('_JEXEC') or die('Restricted access');
 /**
  * Class exists checking
  */
-if (!class_exists('plgSystemZt')) {
+if (!class_exists('plgSystemZt'))
+{
 
     /**
      * Zoo Framework entrypoint plugin
      */
-    class plgSystemZt extends JPlugin {
+    class plgSystemZt extends JPlugin
+    {
 
-        public function __construct(&$subject, $config = array()) {
+        public function __construct(&$subject, $config = array())
+        {
             parent::__construct($subject, $config);
             require_once __DIR__ . '/core/bootstrap.php';
         }
 
-        public function onAfterDispatch() {
+        public function onAfterDispatch()
+        {
             $input = JFactory::getApplication()->input;
             $ztCommand = $input->getCmd('zt_cmd');
-            $ztCommand = explode('.', $ztCommand);
-            if ($ztCommand[0]) {
-                if ($ztCommand[0] = 'ajax') {
+
+            if ($ztCommand)
+            {
+                if ($ztCommand = 'ajax')
+                {
                     $class = $input->get('zt_namespace') . 'HelperAjax';
                     $task = $input->get('zt_task');
-                    if (class_exists($class)) {
+                    if (class_exists($class))
+                    {
                         call_user_func(array($class, $task));
-                        $ajax = ZtAjax::getInstance();                        
+                        $ajax = ZtAjax::getInstance();
                         $ajax->response();
                     }
                 }
